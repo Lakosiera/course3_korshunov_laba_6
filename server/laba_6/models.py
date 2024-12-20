e951be6f0da7ed693bc67d72ac0d6f27d8631b67
Lakosiera <lakosiera@mail.ru>
2024-12-20 18:27:18 +0500

    модели для кредитного банка

--- server/laba_6/models.py
+++ server/laba_6/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(
+        verbose_name="Имя",  # название столбца в админке и ошибках
+        max_length=30,  # максимальная длина текста
+        help_text="Имя",
+    )
+    email = models.EmailField(
+        verbose_name="Почта",  # название столбца в админке и ошибках
+        max_length=50,  # максимальная длина текста
+        help_text="Почта",
+    )
+    address = models.CharField(
+        verbose_name="Адрес",  # название столбца в админке и ошибках
+        max_length=50,  # максимальная длина текста
+        help_text="Адрес",
+    )
+
+
+class Deposit(models.Model):
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.CASCADE,
+    )
+    amount = models.IntegerField(
+        verbose_name="Сумма",  # название столбца в админке и ошибках
+        help_text="Сумма",
+    )
+
+
+class Loan(models.Model):
+    customer = models.ForeignKey(
+        Customer,
+        on_delete=models.CASCADE,
+    )
+    amount = models.IntegerField(
+        verbose_name="Сумма",  # название столбца в админке и ошибках
+        help_text="Сумма",
+    )
+    purpose = models.CharField(
+        verbose_name="Цель кредита",  # название столбца в админке и ошибках
+        max_length=50,  # максимальная длина текста
+        help_text="Цель кредита",
+    )
+    submitted_on = models.DateField(
+        verbose_name="Представлено",  # название столбца в админке и ошибках
+        help_text="Представлено",
+    )
+    disbursement_on = models.DateField(
+        verbose_name="Выплата по",  # название столбца в админке и ошибках
+        help_text="Выплата по",
+    )
--- server/laba_6/views.py
+++ server/laba_6/views.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, permissions
+from .serializers import UserSerializer, GroupSerializer
 
-# пример простейшей вьющки
-def hello_world(request):
-    # простейший вывод html страницы
-    return HttpResponse("Hello, world!")
 
-# вьюшка главной страници
-def index(request):
-    # передаем данные контекста
-    context = {
-        # параметр заголовока
-        "name": "Laba 6 - API Кредитный банк",
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    }
-    # ренедр вьюшки в html страницу
-    return render(request, "index.html", context)
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
