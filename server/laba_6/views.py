0d1edb58c0f87f529d3e9d9823d8eff9a7f1dd71
Lakosiera <lakosiera@mail.ru>
2024-12-20 22:46:55 +0500

    swagger

--- server/laba_6/views.py
+++ server/laba_6/views.py
@@ -23,6 +23,7 @@ class GroupViewSet(viewsets.ModelViewSet):
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class DepositViewSet(viewsets.ModelViewSet):
